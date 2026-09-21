ac7935cf0ac667bcd63a07e2a5a2a8a4171afd41
Fathan Muhammad Faqih <74785057+Churma16@users.noreply.github.com>
2026-09-21 18:08:37 +0700

    test(i18n): verify Indonesian translation coverage

---
+++ src/i18n/locales/id.test.js
@@ -0,0 +1,15 @@
+import translationEN from './en';
+import translationID from './id';
+
+describe('Indonesian translation resource', () => {
+  test('defines every translation key available in English', () => {
+    expect(Object.keys(translationID).sort()).toEqual(Object.keys(translationEN).sort());
+  });
+
+  test('contains representative native Indonesian translations', () => {
+    expect(translationID.settings).toBe('Pengaturan');
+    expect(translationID.addNewItem).toBe('Tambah Barang Baru');
+    expect(translationID.totalDailyCost).toBe('Total Biaya Harian');
+    expect(translationID.selectLanguage).toBe('Pilih Bahasa');
+  });
+});
